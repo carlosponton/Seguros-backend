9542fa30ced0dbbaf8e3c5da974c7eda3c8d2e87
Carlos Pontón <carlos.ponton2@gmail.com>
2018-11-15 19:44:56 -0500

    Create models

---
+++ Seguros/Models/Policy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seguros.Models
+{
+    [Flags]
+    public enum Danger
+    {
+        Bajo,
+        Medio,
+        MedioAlto,
+        Alto
+    }
+
+    public class Policy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int Period { get; set; }
+        public Danger Danger { get; set; }
+
+        public int TypeId { get; set; }
+        public Type Type { get; set; }
+
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+    }
+
+}
---
+++ Seguros/Models/Type.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seguros.Models
+{
+    public class Type
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public double Percentage { get; set; }
+    }
+
+}
